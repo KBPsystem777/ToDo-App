773753cd5856188fb861f333c034a9b2c241df0c
kbpsystem777 <koleen.bp@outlook.com>
2019-03-27 04:55:06 +0800

    building footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => (
+    <div className="text-center">
+        <p>Built with React MDB by <a href="https://www.koleenbp.com">Koleen BP</a></p>
+    </div>
+)
+
+export default Footer
